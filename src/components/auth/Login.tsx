216dd9c7c7ef63022fced2248bfef6289fa57f12
Michael <guay.me@gmail.com>
2023-09-17 19:20:37 -0400

    Login functionality

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
+  const { login, error } = useLogin();
+
   return (
     <>
-      <Auth submitLabel="Login" onSubmit={async () => {}}>
+      <Auth submitLabel="Login" onSubmit={(request) => login(request)}>
         <Link to={"/signup"} style={{ alignSelf: "center" }}>
           <MUILink>Signup</MUILink>
         </Link>
---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { API_URL } from "../constants/urls";
+import client from "../constants/apollo-client";
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+const useLogin = () => {
+  const [error, setError] = useState<boolean>();
+
+  const login = async (request: LoginRequest) => {
+    const res = await fetch(`${API_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+    if (!res.ok) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    await client.refetchQueries({ include: "active" });
+  };
+
+  return { login, error };
+};
+
+export { useLogin };
