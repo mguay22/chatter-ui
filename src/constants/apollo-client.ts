1c1fa106cdd45c44e300c29e94dc7f07f253a18c
Michael <guay.me@gmail.com>
2023-09-23 14:01:44 -0400

    Add logout link

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,9 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { API_URL } from "./urls";
+import excludedRoutes from "./excluded-routes";
+import router from "../components/Routes";
+
+const logoutLink = onError((error) => {
+  if (
+    error.graphQLErrors?.length &&
+    (error.graphQLErrors[0].extensions.originalError as any).statusCode === 401
+  ) {
+    if (!excludedRoutes.includes(window.location.pathname)) {
+      router.navigate("/login");
+      client.resetStore();
+    }
+  }
+});
+
+const httpLink = new HttpLink({ uri: `${API_URL}/graphql` });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: `${API_URL}/graphql`,
+  link: logoutLink.concat(httpLink),
 });
 
 export default client;
