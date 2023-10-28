61e2a71e4f9eb485d02fb09cc8f1e0ca1ccd7b05
Michael <guay.me@gmail.com>
2023-10-28 17:12:56 -0400

    Update cache

--- src/hooks/useCreateChat.ts
+++ src/hooks/useCreateChat.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@apollo/client";
 import { graphql } from "../gql";
+import { ChatFragment } from "../fragments/chat.fragment";
 
 const createChatDocument = graphql(`
   mutation CreateChat($createChatInput: CreateChatInput!) {
@@ -10,7 +11,21 @@ const createChatDocument = graphql(`
 `);
 
 const useCreateChat = () => {
-  return useMutation(createChatDocument);
+  return useMutation(createChatDocument, {
+    update(cache, { data }) {
+      cache.modify({
+        fields: {
+          chats(existingChats = []) {
+            const newChatRef = cache.writeFragment({
+              data: data?.createChat,
+              fragment: ChatFragment,
+            });
+            return [...existingChats, newChatRef];
+          },
+        },
+      });
+    },
+  });
 };
 
 export { useCreateChat };
