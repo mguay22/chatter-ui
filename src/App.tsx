c1fbdd361e3bd865f2fbed904d3f8a48617d6ee9
Michael <guay.me@gmail.com>
2023-11-04 12:36:45 -0400

    Finish chat

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,15 @@ const darkTheme = createTheme({
 const App = () => {
   const { path } = usePath();
 
+  const showChatList = path === "/" || path.includes("chats");
+
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Header />
         <Guard>
-          {path === "/" ? (
+          {showChatList ? (
             <Grid container>
               <Grid item md={3}>
                 <ChatList />
@@ -51,7 +53,7 @@ const App = () => {
 
 const Routes = () => {
   return (
-    <Container>
+    <Container sx={{ height: "100%" }}>
       <RouterProvider router={router} />
     </Container>
   );
--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,11 +1,35 @@
 import { useParams } from "react-router-dom";
 import { useGetChat } from "../../hooks/useGetChat";
+import { Divider, IconButton, InputBase, Paper, Stack } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 const Chat = () => {
   const params = useParams();
   const { data } = useGetChat({ _id: params._id! });
 
-  return <h1>{data?.chat.name}</h1>;
+  return (
+    <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
+      <h1>{data?.chat.name}</h1>
+      <Paper
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          justifySelf: "flex-end",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1, width: "100%" }}
+          placeholder="Message"
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: "10px" }}>
+          <SendIcon />
+        </IconButton>
+      </Paper>
+    </Stack>
+  );
 };
 
 export default Chat;
